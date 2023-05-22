4da9847b82b78470c38325e8f2b676cb88414387
Yang Zhang <credo_zhang@163.com>
2023-05-22 11:29:27 +0200

    cleaning modules

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-import pkg_resources, os, sys, json
+import pkg_resources, os, sys, json, datetime, builtins
 
 configfile = pkg_resources.resource_filename("BetaPose", "myconfig.json")
 if os.path.isfile(configfile): 
@@ -8,6 +8,19 @@ else:
   print("Not found the config file", file=sys.stderr)
 
 from . import test
+
+########################################################
+def logit(function):
+  def adddate(*arg, **kwarg):
+    timestamp = datetime.datetime.now().strftime('%y-%m-%dT%H:%M:%S')
+    builtins.print(f"{timestamp:20s}: ", end="")
+    function(*arg, **kwarg)
+  return adddate
+
+@logit
+def printit(*arg, **kwarg):
+  builtins.print(*arg, **kwarg)
+
 
 # import pytraj as pt
 # import numpy as np 
---
+++ scripts/create_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l 
+
+# ${1}
+conda create -n testenv python=3.8 -y
+conda activate testenv
+conda install -c conda-forge ambertools
+
+
+pip install 
+
+
