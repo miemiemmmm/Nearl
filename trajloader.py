c945e32a77e054ef73bc50baf69c261ad134798d
Yang Zhang <y.zhang@bioc.uzh.uch>
2023-07-03 17:52:29 +0200

    Added PDBbind complex preparation script and better data_io/trajectory loader and RFScore data preparation

--- trajloader.py
+++ trajloader.py
@@ -1,18 +1,22 @@
 import time;
 import pytraj as pt;
 import numpy as np;
-from . import utils, CONFIG
+from . import utils, CONFIG, printit;
+
+_verbose = CONFIG.get("verbose", False);
 
 # Loader of trajectories for iteration
-# Basic way of using: 
-
+# Principle way of using it:
+"""
+Example:
+  >>> trajloader = TrajectoryLoader(trajs, tops, **kwarg)
+  >>> for traj in trajloader:
+  >>>   # Do something with traj
+"""
 class TrajectoryLoader: 
   def __init__(self, trajs, tops, **kwarg):
     """
     Systematically load trajectories for further processing
-    Example:
-    >>> for traj in trajloader:
-    >>>   # Do something with traj
     Args:
         trajs (str or list): Trajectory file names
         tops (str or list): Topology file names
@@ -64,79 +68,81 @@ class Trajectory(pt.Trajectory):
       pdbfile:  pdb file
       **kwarg:  keyword arguments
     """
+    if _verbose:
+      printit(f"Module {self.__class__.__name__}: Loading trajectory {trajfile} with topology {pdbfile}");
     st = time.perf_counter();
-    super().__init__(top=pdbfile);
     # Set the keyword arguments for slicing/masking trajectory;
     stride = kwarg.get("stride", None)
     frame_indices = kwarg.get("frame_indices", None)
     mask = kwarg.get("mask", None)
-    if CONFIG.get("verbose", False):
-      print(f"Module {self.__class__.__name__}: Loading trajectory {trajfile} with topology {pdbfile}");
-      print(f"Module {self.__class__.__name__}: stride: {stride}, frame_indices: {frame_indices}, mask: {mask}")
-    traj = pt.io.load_traj(trajfile, self.top, stride=stride);
-    # Do the slicing and Masking if needed
-    if stride is not None:
-      if mask is None:
-        buffer_traj = traj[:]
-      else:
-        buffer_traj = traj[mask]
-    else:
-      frame_indices_ = frame_indices
-      # Convert the frame_indices to list if it is tuple
-      if isinstance(frame_indices_, tuple):
-        frame_indices_ = list(frame_indices_)
-      # Load all frames if frame_indices_ is None
-      if frame_indices_ is None and mask is None:
-        buffer_traj = traj[:]
-      elif frame_indices_ is None and mask is not None:
-        # load all frames with given mask
-        # eg: traj['@CA']
-        buffer_traj = traj[mask]
-      elif frame_indices_ is not None and mask is None:
-        # eg. traj[[0, 3, 7]]
-        buffer_traj = traj[frame_indices_]
-      else:
-        # eg. traj[[0, 3, 7], '@CA']
-        buffer_traj = traj[frame_indices_, mask]
-    # Reassign the trajectory if there is slicing or masking of the trajectory
-    if (self.n_frames != buffer_traj.n_frames) or (self.n_atoms != buffer_traj.n_atoms):
-      if CONFIG.get("verbose", False):
-        print(f"Module {self.__class__.__name__}: Reassigning the trajectory; ");
-      self.top = buffer_traj.top;
-      self._xyz = buffer_traj._xyz;
-      self._boxes = buffer_traj._boxes;
-      self.forces = buffer_traj.forces;
-      self.velocities = buffer_traj.velocities;
-      self.time = buffer_traj.time;
-      self._life_holder = buffer_traj._life_holder;
-      self._frame_holder = buffer_traj._frame_holder;
 
-    self.activeframe = 0;
-    if CONFIG.get("verbose", False):
-      print(f"Module {self.__class__.__name__}: Trajectory loaded in {time.perf_counter() - st:.2f} seconds");
-  def copy(self):
-    thecopy = pt.Trajectory();
-    thecopy.top = self.top.copy()
-    thecopy.xyz = self._xyz.copy()
-    return thecopy
+    # NOTE: If both stride and frame_indices are given, stride will be respected;
+    # NOTE: If none of stride or frame_indices are given, all frames will be loaded;
+    tmptraj = pt.load(trajfile, pdbfile, stride=stride, frame_indices=frame_indices);
+    timeinfo = tmptraj.time;
+    boxinfo = tmptraj._boxes;
 
-  def cluster_pairwise(self, mask, **kwarg): 
+    # NOTE: Adding mask in the first pt.load function causes lose of time information;
+    if mask is not None:
+      tmptraj = tmptraj[mask];
+    top = tmptraj.top;
+    xyz = tmptraj.xyz;
+
+    # Set basic attributes for pytraj.Trajectory;
+    super().__init__(xyz=xyz, top=top, velocity=tmptraj.velocities, force=tmptraj.forces);
+    self._boxes = boxinfo;
+    self.time   = timeinfo;
+    self._life_holder = tmptraj._life_holder;
+    self._frame_holder = tmptraj._frame_holder;
+
+    # Personalized attributes to facilitate further trajectory processing;
+    self.top_filename  = pdbfile;
+    self.traj_filename = trajfile;
+    self.mask = mask;
+
+    self._active_index = 0;
+    self._active_frame = self[0];
+    # print("=====>", len(self))
+
+    if _verbose:
+      printit(f"Module {self.__class__.__name__}: stride: {stride}; frame_indices: {frame_indices}; mask: {mask}");
+      printit(f"Module {self.__class__.__name__}: Trajectory loaded in {time.perf_counter() - st:.2f} seconds");
+
+  @property
+  def active_frame(self):
+    return self._active_index, self._active_frame;
+  @active_frame.setter
+  def active_frame(self, index):
+    try:
+      self._active_index = index;
+      self._active_frame = self[index];
+    except IndexError:
+      self._active_index = 0;
+      self._active_frame = self[0];
+      printit(f"Module {self.__class__.__name__}: Index {index} out of range ({len(self)}). Reset active to frame 0.");
+
+  def compute_closest_pairs_distance(self, mask, **kwarg):
     if "countermask" in kwarg.keys():
       countermask = kwarg["countermask"]
-      pdist, y = utils.PairwiseDistance(self.traj, f"{mask}&!@H=", f"{countermask}&!@H=", use_mean=True);
-    else: 
-      pdist, y = utils.PairwiseDistance(self.traj, f"{mask}&!@H=", f"{mask}<@6&!{mask}&@C,CA,CB,N,O", use_mean=True);
-    clusters = cluster.ClusterAgglomerative(pdist, 10); 
+      pdist, pdist_info = utils.PairwiseDistance(self, f"{mask}&!@H=", f"{countermask}&!@H=", use_mean=True);
+    else:
+      pdist, pdist_info = utils.PairwiseDistance(self, f"{mask}&!@H=", f"{mask}<@6&!{mask}&@C,CA,CB,N,O", use_mean=True);
+    self.pdist = pdist;
+    self.pdist_info = pdist_info;
+    return pdist, pdist_info;
+
+  ############################################
+  ############################################
+  ############################################
+  ############################################
+  def cluster_pairwise(self, cluster_nr=10, **kwarg):
+    clusters = cluster.ClusterAgglomerative(pdist, cluster_nr);
     cluster_rand = cluster.RandomPerCluster(clusters, number=1); 
     self.frames = cluster_rand; 
     return self.frames
     
-  def cluster_rmsd(self):
-    pass
-    
   def slicetraj(self, frameindices):
     frameindices = np.array(frameindices).astype(int)
-
     
   def cluster(self, method="", **kwarg):
     if len(method) == 0:
@@ -174,6 +180,11 @@ class Trajectory(pt.Trajectory):
     self.testtraj = newtraj; 
     pt.write_traj("/tmp/test.pdb", newtraj, overwrite=True)
 
+  # def copy(self):
+  #   thecopy = pt.Trajectory();
+  #   thecopy.top = self.top.copy()
+  #   thecopy.xyz = self._xyz.copy()
+  #   return thecopy
 
 
 
