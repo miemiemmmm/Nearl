4c59cb32cb02f18c7491079c16cf037a48be8838
Yang Zhang <credo_zhang@163.com>
2023-08-24 12:41:28 +0200

    Updated the Github Action workflow and depends on mamba for more efficient execution

--- build_dev.sh
+++ build_dev.sh
@@ -6,7 +6,7 @@
 # 3. Install from source distribution as a test
 # 4. Switch back to editable mode
 
-env_name="mlenv";
+env_name=${1:-"mlenv"};
 
 tstamp(){
   echo ">>>> $(date '+%d.%m.%Y %H.%M.%S') : "
@@ -14,33 +14,36 @@ tstamp(){
 logit(){
   echo ">>>> $(date '+%d.%m.%Y %H.%M.%S') : $@"
 }
-# Find and initialize conda, then find and activate the desired conda environment
-if [ ${#env_name} -eq 0 ]; then
-  logit "Please define a environment name: bash ${0} <environment name>";
-  exit -1;
-fi
-if ! command -v conda &> /dev/null; then
-  logit "Conda could not be found, please install it first";
-  exit -1;
-fi
-if [ -f $(conda info --base)/etc/profile.d/conda.sh ]; then
-  logit "Initializing conda";
-  source $(conda info --base)/etc/profile.d/conda.sh;
-else
-  logit "Conda init script not found, please check if conda is installed correctly";
-  exit -1;
-fi
-foundenv=$(conda info --envs | egrep "/${env_name}$" | awk '{print $1}')
-if [ ${#foundenv} -eq 0 ]; then
-  logit "Warning: Cannot found the environment ${env_name} to install the package, please check if the environment is created correctly";
-  exit -1;
-fi
-conda activate ${env_name};
+
 if [[ ${CONDA_DEFAULT_ENV} != ${env_name} ]]; then
-  logit "Failed to activate the environment ${env_name}";
-  exit -1;
-else
-  logit "Running in the environment ${CONDA_DEFAULT_ENV}";
+  # If the corresponding conda environment is not activated, activate it
+  if [ ${#env_name} -eq 0 ]; then
+    logit "Please define a environment name: bash ${0} <environment name>";
+    exit -1;
+  fi
+  if ! command -v conda &> /dev/null; then
+    logit "Conda could not be found, please install it first";
+    exit -1;
+  fi
+  if [ -f $(conda info --base)/etc/profile.d/conda.sh ]; then
+    logit "Initializing conda";
+    source $(conda info --base)/etc/profile.d/conda.sh;
+  else
+    logit "Conda init script not found, please check if conda is installed correctly";
+    exit -1;
+  fi
+  foundenv=$(conda info --envs | egrep "/${env_name}$" | awk '{print $1}')
+  if [ ${#foundenv} -eq 0 ]; then
+    logit "Warning: Cannot found the environment ${env_name} to install the package, please check if the environment is created correctly";
+    exit -1;
+  fi
+  conda activate ${env_name};
+  if [[ ${CONDA_DEFAULT_ENV} != ${env_name} ]]; then
+    logit "Failed to activate the environment ${env_name}";
+    exit -1;
+  else
+    logit "Running in the environment ${CONDA_DEFAULT_ENV}";
+  fi
 fi
 
 current_path=$(dirname $(realpath ${0}));
