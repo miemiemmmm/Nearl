a8e90d6ba9be54897ef7dca76957c1f877d19758
Yang Zhang <y.zhang@bioc.uzh.ch>
2024-03-01 17:13:09 +0100

    Adjusted the script structures.

---
+++ scripts/misc/install_mamba.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# Install the micromamba to be able to fast deploy the python environment
+# The default base directory is the current working directory
+# Usage: bash install_mamba.sh <base directory>
+if [ ${#1} -eq 0 ]; then
+  echo "Usage: bash install_mamba.sh <base directory>" 1>&2
+  exit 0
+fi
+set -eu
+
+
+basedir=${1:-./micromamba}
+basedir=$(realpath $basedir)
+mkdir -p ${basedir}/bin
+echo "Installing micromamba to ${basedir}"
+export BIN_FOLDER="${basedir}/bin"
+export PREFIXLOCATION="${basedir}"
+export INIT_YES="N"
+export CONDA_FORGE_YES="Y"
+
+
+ARCH="$(uname -m)"
+OS="$(uname)"
+
+if [[ "$OS" == "Linux" ]]; then
+  PLATFORM="linux"
+  if [[ "$ARCH" == "aarch64" ]]; then
+    ARCH="aarch64"
+  elif [[ $ARCH == "ppc64le" ]]; then
+    ARCH="ppc64le"
+  else
+    ARCH="64"
+  fi
+elif [[ "$OS" == "Darwin" ]]; then
+  PLATFORM="osx"
+  if [[ "$ARCH" == "arm64" ]]; then
+    ARCH="arm64"
+  else
+    ARCH="64"
+  fi
+elif [[ "$OS" =~ "NT" ]]; then
+  PLATFORM="win"
+  ARCH="64"
+else
+  echo "Failed to detect your OS" >&2
+  exit 1
+fi
+
+
+RELEASE_URL="https://github.com/mamba-org/micromamba-releases/releases/latest/download/micromamba-${PLATFORM}-${ARCH}"
+echo "Basic info: $PLATFORM $ARCH $OS"
+echo "Config settings: $BIN_FOLDER $PREFIXLOCATION $INIT_YES $CONDA_FORGE_YES"
+echo "Release url: "$RELEASE_URL
+
+
+curl "${RELEASE_URL}" -o "${BIN_FOLDER}/micromamba" -fsSL --compressed ${CURL_OPTS:-}
+chmod +x "${BIN_FOLDER}/micromamba"
+
+# Initializing shell
+if [[ "$INIT_YES" == "" || "$INIT_YES" == "y" || "$INIT_YES" == "Y" || "$INIT_YES" == "yes" ]]; then
+  case "$("${BIN_FOLDER}/micromamba" --version)" in
+    1.*|0.*)
+      "${BIN_FOLDER}/micromamba" shell init -p "${PREFIXLOCATION}"
+      ;;
+    *)
+      "${BIN_FOLDER}/micromamba" shell init --root-prefix "${PREFIXLOCATION}"
+      ;;
+  esac
+
+  echo "Please restart your shell to activate micromamba or run the following:\n"
+  echo "  source ~/.bashrc (or ~/.zshrc, ...)"
+fi
+
+
+# Initializing conda-forge
+if [[ "$CONDA_FORGE_YES" == "" || "$CONDA_FORGE_YES" == "y" || "$CONDA_FORGE_YES" == "Y" || "$CONDA_FORGE_YES" == "yes" ]]; then
+  "${BIN_FOLDER}/micromamba" config append channels conda-forge
+  "${BIN_FOLDER}/micromamba" config append channels nodefaults
+  "${BIN_FOLDER}/micromamba" config set channel_priority strict
+fi
+
+
+# Several steps to initialize and hook the micromamba
+export PATH="$(realpath ${BIN_FOLDER}):${PATH}";
+export MAMBA_EXE=${BIN_FOLDER}/micromamba;
+export MAMBA_ROOT_PREFIX=${PREFIXLOCATION};
+eval "$(micromamba shell hook --shell bash --root-prefix ${PREFIXLOCATION})";
+
+
+# Finally check if micromamba is able to create a new environment and activate it
+echo "Installing a test environment"
+micromamba create --name TEST_ENV python=3.9.17 -c conda-forge -q -y
+micromamba activate TEST_ENV
+if micromamba env list | grep TEST_ENV | grep "*" &> /dev/null; then
+  echo "Great! Micromamba installation successful and test environment activated";
+  echo -e "Y\n" | micromamba env remove --name TEST_ENV -q -y;
+else
+  echo "No!!!! Micromamba cannot activate the TEST_ENV environment";
+  exit 1
+fi
+
+# Create a script to load micromamba
+loadmamba_b64="YmFzZWRpcj0kezE6LVRFTVBMQVRFRElSfQpiYXNlZGlyPSQocmVhbHBhdGggJGJhc2VkaXIpCkJJTl9GT0xERVI9IiR7YmFzZWRpcn0vYmluIgpQUkVGSVhMT0NBVElPTj0iJHtiYXNlZGlyfSIKZXhwb3J0IFBBVEg9IiQocmVhbHBhdGggJHtCSU5fRk9MREVSfSk6JHtQQVRIfSIKZXhwb3J0IE1BTUJBX0VYRT0ke0JJTl9GT0xERVJ9L21pY3JvbWFtYmE7CmV4cG9ydCBNQU1CQV9ST09UX1BSRUZJWD0ke1BSRUZJWExPQ0FUSU9OfTsKZXZhbCAiJChtaWNyb21hbWJhIHNoZWxsIGhvb2sgLS1zaGVsbCBiYXNoIC0tcm9vdC1wcmVmaXggJHtQUkVGSVhMT0NBVElPTn0pIgo="
+python3 -c "import base64; tmp_str=base64.b64decode(\"${loadmamba_b64}\".encode('utf-8')).decode('utf-8'); print(tmp_str)" | sed "s|TEMPLATEDIR|${basedir}|g" > ${basedir}/bin/loadmamba
+chmod +x ${basedir}/bin/loadmamba
+
+echo "If you wish to load micromamba upon opening a new shell, please add the following lines to your ~/.bashrc or ~/.zshrc"
+echo -e '    source '${basedir}'/bin/loadmamba'
+
---
+++ scripts/training/train_3DCNN.py
@@ -0,0 +1,68 @@
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+from nearl import models, printit, utils
+
+import dask.array as da
+
+
+# Define the chunks size according to your memory limit
+
+
+BATCH_SIZE = 32
+EPOCHS = 10
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("The device is: ", DEVICE)
+
+# rf_training_data = np.random.normal(size=(20000, 3, 32, 32, 32));
+st = time.perf_counter()
+chunks = (1000, 3, 32, 32, 32)  # Adjust this to a suitable size according to your memory capacity
+rf_training_data = da.random.normal(size=(12000, 3, 32, 32, 32), chunks=chunks)
+rf_training_data.compute()
+rf_training_data = np.asarray(rf_training_data, dtype=np.float32)
+print("Input data preared: ", rf_training_data.shape, f"Time elapsed: {time.perf_counter()-st:.3f} s")
+# rng = np.random.default_rng()
+# rf_training_data = rng.normal(size=(20000, 3, 32, 32, 32), dtype='float32')
+label_training_data = np.random.normal(size=(12000, 1))
+
+dataset = TensorDataset(torch.from_numpy(rf_training_data).to(dtype=torch.float32), torch.from_numpy(label_training_data).to(dtype=torch.float32))
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+model = models.CNN3D().to(DEVICE)
+loss_fn = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+st = time.perf_counter()
+for epoch in range(EPOCHS):
+  print(f"Epoch {epoch+1}\n------------------------------------------------")
+  running_loss = 0.0
+  for i, data in enumerate(dataloader, 0):
+    inputs, labels = data
+    inputs = inputs.to(DEVICE)
+    labels = labels.to(DEVICE)
+    optimizer.zero_grad()
+    outputs = model(inputs)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    running_loss += loss.item()
+    if ((i+1) % 100) == 0:
+      print(f"Epoch{epoch+1}, batch{i+1:5d}: running loss: {running_loss:.3f}");
+      running_loss = 0.0
+
+print("Finished Training, Total time elapsed: ", time.perf_counter()-st, " seconds");
+
+
+
+
+
+
+
+
+
+
