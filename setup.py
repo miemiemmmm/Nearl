d9cfd7af9ec71b235842c4483ab4e2f15c2981fe
Yang Zhang <credo_zhang@163.com>
2026-09-01 17:40:08 +0200

    Fix the project not compiled in editable dev mode

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 import os
 import subprocess
-import shutil
 from setuptools import setup, find_packages, Extension
-from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext as _build_ext
 
-class install_Nearl(install):
-  def run(self): 
-    print("#### Running the install command")
-    os.chdir("src")
-    subprocess.check_call(["make", "all_actions"])
-    os.chdir("..")
-    if os.path.isfile("./src/all_actions.so"):
-      if os.path.isdir("./build/lib/nearl"):
-        shutil.copy2("./src/all_actions.so", "./build/lib/nearl/all_actions.so")
-      elif os.path.isdir("./nearl"):
-        shutil.copy2("./src/all_actions.so", "./nearl/all_actions.so")
-      else: 
-        raise Exception("The build directory is not found; Please check the build process")
-    else:
-      raise Exception("The shared object file (all_actions.so) is not found; Please check the build process")
-    install.run(self)
+SRC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "src")
+
+# build_ext (unlike the old "install" command) runs for editable installs too.
+class build_ext_nearl(_build_ext):
+  def build_extension(self, ext):
+    if ext.name != "nearl.all_actions":
+      return super().build_extension(ext)
+    subprocess.check_call(["make", "all_actions"], cwd=SRC_DIR)
+    built_so = os.path.join(SRC_DIR, "all_actions.so")
+    if not os.path.isfile(built_so):
+      raise Exception(f"The shared object file ({built_so}) was not produced; please check the build process")
+    target = self.get_ext_fullpath(ext.name)
+    os.makedirs(os.path.dirname(target), exist_ok=True)
+    self.copy_file(built_so, target)
 
 
 setup_params = dict(
-  cmdclass = {"install": install_Nearl},
+  cmdclass = {"build_ext": build_ext_nearl},
   packages = find_packages(),
+  ext_modules = [Extension("nearl.all_actions", sources=[])],
   zip_safe = False,
 )
 
