f0e00fa441498c655020f61cfccaf5ff65d6d4e3
mumairsadiq <mumairsadiq1@gmail.com>
2026-09-08 10:04:40 +0200

    Create slots for GPU/Pinned Host buffers

--- src/gpuutils.cuh
+++ src/gpuutils.cuh
@@ -94,6 +94,7 @@ public:
   void cancel_call() noexcept;
   bool pending() const { return pending_; }
   void *stage_input(const void *source, size_t bytes, size_t slot);
+  void *get_host_buffer(size_t min_bytes, size_t slot);
 
 private:
   struct Buffer {
